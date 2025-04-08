1731740b3bb95d46245682014f2fc24966d10007
christinedn <christinedn7@gmail.com>
2025-04-08 11:17:45 -0700

    Initial Commit

---
+++ RestaurantViewer/RestaurantViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantViewerApp.swift
+//  RestaurantViewer
+//
+//  Created by Christine on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestaurantViewerTests/RestaurantViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantViewerTests.swift
+//  RestaurantViewerTests
+//
+//  Created by Christine on 4/8/25.
+//
+
+import Testing
+@testable import RestaurantViewer
+
+struct RestaurantViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
